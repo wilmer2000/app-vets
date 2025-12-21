189a767a7809827d6e7fb3ad6e8fbba50bc04b23
Wilmer Blanco <wilmervzla2000@gmail.com>
2025-12-21 12:34:35 -0300

    Agregado auth module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller.js';
-import { AppService } from './app.service.js';
 import { PrismaService } from '../prisma/prisma.service.js';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [PrismaService],
 })
 export class AppModule {}
