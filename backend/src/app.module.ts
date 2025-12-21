547490996351b982cd74025675d6a773d1d126b2
Wilmer Blanco <wilmervzla2000@gmail.com>
2025-12-21 00:20:47 -0300

    Prisma instalado

---
+++ backend/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
+import { PrismaService } from '../prisma/prisma.service.js';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService, PrismaService],
+})
+export class AppModule {}
