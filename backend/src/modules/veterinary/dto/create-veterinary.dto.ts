8e5c88c174f800007b1f2391f42a0f29e4c2e586
Wilmer Blanco <wilmervzla2000@gmail.com>
2025-12-30 12:18:46 -0300

    refactor(new-model-db): update DTOs for veterinary profiles
    - Refactored `CreateVeterinaryDto` and `UpdateVeterinaryDto` to include validation and transformation logic for all fields.
    - Ensured consistency across DTOs by applying the same validation and transformation rules.

--- backend/src/modules/veterinary/dto/create-veterinary.dto.ts
+++ backend/src/modules/veterinary/dto/create-veterinary.dto.ts
@@ -1,28 +1,46 @@
 import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class CreateVeterinaryDto {
   @IsNotEmpty()
+  @IsString()
+  @Transform(({ value }) => value.trim())
   name: string;
 
+  @IsNotEmpty()
+  @IsString()
+  @Transform(({ value }) => value.trim())
   @IsEmail()
   email: string;
 
+  @IsNotEmpty()
   @IsString()
+  @Transform(({ value }) => value.trim())
   description: string;
 
+  @IsNotEmpty()
   @IsString()
+  @Transform(({ value }) => value.trim())
   phone: string;
 
+  @IsNotEmpty()
   @IsString()
+  @Transform(({ value }) => value.trim())
   street: string;
 
+  @IsNotEmpty()
   @IsString()
+  @Transform(({ value }) => value.trim())
   city: string;
 
+  @IsNotEmpty()
   @IsString()
+  @Transform(({ value }) => value.trim())
   country: string;
 
+  @IsNotEmpty()
   @IsString()
+  @Transform(({ value }) => value.trim())
   businessHours: string;
 
   @IsBoolean()
