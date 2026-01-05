e03bf357d5bb7866750bf738c26185a94cd80b70
Wilmer Blanco <wilmervzla2000@gmail.com>
2026-01-05 00:12:57 -0300

    refactor(main): simplify layout and integrate components
    - Removed unnecessary containers in pet pages.
    - Integrated PetProfileComponent into PetPage for better organization.

--- frontend/src/app/pages/pet/pet.component.ts
+++ frontend/src/app/pages/pet/pet.component.ts
@@ -1,10 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ContainerComponent } from '../../shared/components/container/container.component';
 
 @Component({
   selector: 'app-pet',
-  imports: [RouterOutlet, ContainerComponent],
+  imports: [RouterOutlet],
   templateUrl: './pet.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
