d2578e4c615ffbf068573d69f0c461ce78f5f886
Wilmer Blanco <wilmervzla2000@gmail.com>
2026-01-27 23:19:21 -0300

    - Updated `IconComponent` to include additional properties for icon size, stroke width, and absolute stroke width. - Added styles to ensure the icon component is displayed as an inline block. - Refactored `AvatarComponent` to use the new `IconComponent`. - Updated templates in `NavbarItemComponent.html` to align icons with their labels.

--- frontend/src/app/core/components/avatar/avatar.component.ts
+++ frontend/src/app/core/components/avatar/avatar.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { IconComponent } from '../../../shared/components/icon/icon.component';
 
 @Component({
   selector: 'app-avatar',
-  imports: [],
+  imports: [IconComponent],
   templateUrl: './avatar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
--- frontend/src/app/shared/components/icon/icon.component.ts
+++ frontend/src/app/shared/components/icon/icon.component.ts
@@ -4,11 +4,25 @@ import { icons, LucideAngularModule } from 'lucide-angular';
 @Component({
   selector: 'app-icon',
   imports: [LucideAngularModule],
-  template: `<i-lucide [name]="iconRef()" [class]="classes()" class="icon block mx-auto" />`,
+  template: `<i-lucide
+    [name]="iconRef()"
+    [size]="size()"
+    [class]="classes()"
+    [strokeWidth]="stroke()"
+    [absoluteStrokeWidth]="true"
+    class="icon"
+  />`,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: `
+    :host {
+      display: inline-block;
+    }
+  `,
 })
 export class IconComponent {
   protected iconRef = computed(() => (icons as any)[this.icon()] || icons.Smile);
   icon = input.required<string>();
   classes = input<string>('');
+  stroke = input<number>(2);
+  size = input<number>(24);
 }
